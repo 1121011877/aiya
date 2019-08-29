43181324549a2ecd39facc5a0ea740b3c4e9b72d
JameMoriarty <G1657643182@163.com>
2019-08-29 12:44:08 +0800

    修改了个人中心不显示头像的问题

--- pages/Dmy/Dmy.js
+++ pages/Dmy/Dmy.js
@@ -38,10 +38,18 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success(res) { }
+          })
+        }
+      }
+    })
 
   },
-
   onShow: function () {
     var that = this
     wx.request({
